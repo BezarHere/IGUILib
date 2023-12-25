c639e09ab181481fcfd46e8ed5dcd98254d82e2d
botatodev <botatodev@gmail.com>
2023-12-26 01:13:37 +0300

    more work

--- igui-lib/src/igui.cpp
+++ igui-lib/src/igui.cpp
@@ -1,7 +1,48 @@
 #include "pch.h"
+#include "igui.h"
+using namespace igui;
+
+// for intelli-sense
+constexpr i32 NodeTSIZE = sizeof( Node ) * 16;
 
 namespace igui
 {
+	Node::Node()
+		: m_type{ NodeType::None } {
+	}
+
+	Node::Node( NodeType type )
+		: m_type{ type } {
+	}
+
+	void Interface::update() {
+	}
+
+	void Interface::draw( const Window *window, Renderer *renderer ) const {
+	}
+
+	index_t Interface::add_node( const Node &node, index_t parent ) {
+		if (node.m_parent != InvalidIndex || !node.m_children.empty())
+			return InvalidIndex;
+
+		const size_t ln = m_nodes.size();
+		m_nodes.push_back( node );
+		if (parent == InvalidIndex)
+		{
+			m_roots.push_back( ln );
+		}
+		else
+		{
+			m_nodes[ parent ].m_children.push_back( ln );
+			m_nodes[ ln ].m_parent = parent;
+		}
+
+		return ln;
+	}
+
+	index_t Interface::transfer_branch( const this_type &old, index_t node_index, index_t parent ) {
+		return InvalidIndex;
+	}
 
 }
 
--- igui-lib/src/igui.h
+++ igui-lib/src/igui.h
@@ -27,7 +27,6 @@ namespace igui
 	typedef int64_t i64;
 
 	typedef size_t index_t;
-
 	constexpr index_t InvalidIndex = (index_t)-1;
 
 	
@@ -36,8 +35,13 @@ namespace igui
 	using Vec2i = ig::Vector2i;
 	using Rectf = ig::Rect2f;
 	using Recti = ig::Rect2i;
+	using Color = ig::Colorf;
 
-	using string = std::string;
+	using Window = ig::Window;
+	using Renderer = ig::Renderer;
+
+	// FIXME: literals will break if 'string' requires a wide string
+	using string = std::string; 
 	template <typename _T>
 	using vector = std::vector<_T>;
 
@@ -59,14 +63,44 @@ namespace igui
 	{
 		StateMask_Enabled = 0x00'00'00'01,
 
+		StateMask_Value   = 0xff'ff'00'00,
+		StateMask_FPValue = StateMask_Value,
+	};
+
+	enum class MouseFilter : i16
+	{
+		Stop,
+		Through,
+		Ignore
+	};
+
+	enum class CursorShape : i16
+	{
+		Arrow,
+		Move,
+		ResizeHorizontal,
+		ResizeVertical,
+		Resize,
+		PointerHand,
 	};
 
 	class NodeFactory
 	{
+	public:
 
+
+
+	private:
+		NodeFactory() = delete;
+		~NodeFactory() = delete;
 	};
 
-	class Style
+	enum SizeFlags : i16
+	{
+		SizeFlags_None = 0
+	};
+
+	struct Style
 	{
 
 	};
@@ -74,16 +108,52 @@ namespace igui
 	class Interface
 	{
 	public:
+		using this_type = Interface;
 
+		void update();
+		void draw(const Window *window, Renderer *renderer) const;
 
+		// to add a node it must be singlet (no parent or children) OR this will fail
+		// if 'parent' is set to InvalidIndex the node will be a root node
+		// [On Error] InvalidIndex will be returned
+		index_t add_node( const Node &node, index_t parent = InvalidIndex );
+
+		// to transfer the branch node (the node at 'node_index') an it children
+		// the branch node will be parented to 'parent'
+		// if 'parent' is set to InvalidIndex the node will be a root node
+		// [On Error] InvalidIndex will be returned
+		index_t transfer_branch( const this_type &old, index_t node_index, index_t parent = InvalidIndex );
+
+		template <typename _PRED>
+		inline index_t find_node( _PRED predicate ) const;
+
+		inline Node &get_node( index_t i ) {
+			return m_nodes[ i ];
+		}
+
+		inline const Node &get_node( index_t i ) const {
+			return m_nodes[ i ];
+		}
+
+		inline const vector<Node> &get_nodes() const {
+			return m_nodes;
+		}
 
 	private:
 		vector<Node> m_nodes;
+		vector<index_t> m_roots;
+		Style m_style;
 	};
 
 	class Node
 	{
+		friend Interface;
 	public:
+		Node();
+		Node( NodeType type );
+
+		inline i32 value() const noexcept;
+		inline float value_fp() const noexcept;
 
 		inline bool enabled() const noexcept {
 			return m_state & StateMask_Enabled;
@@ -95,18 +165,46 @@ namespace igui
 
 	private:
 		NodeType m_type;
-		i64      m_state;
-		i32			 m_flags;
-		string   m_text;
-		Rectf    m_anchors = { 0.f, 0.f, 1.f, 1.f };
+		i64 m_state        = StateMask_Enabled;
+		i32 m_flags        = 0;
+		Rectf m_rect       = { 0.f, 0.f, 32.f, 32.f };
+		Rectf m_anchors    = { 0.f, 0.f, 1.f, 1.f };
+		Vec2f m_pivot      = { 0.f, 0.f };
+		float m_angle      = 0.0f;
+
+		MouseFilter m_mouse_filter = MouseFilter::Stop;
+		CursorShape m_cursor_shape = CursorShape::Arrow;
+
+		string m_text      = "Sample text";
+		string m_tooltip   = "Sample tooltip"; // <- empty tooltip will not be displayed
 
 		struct {
-			i16 h, v;
+			SizeFlags horizontal = SizeFlags_None;
+			SizeFlags vertical   = SizeFlags_None;
 		} m_size_flags;
 
-		vector<index_t> m_children;
-		index_t         m_parent = InvalidIndex;
+		vector<index_t> m_children = {};
+		index_t         m_parent   = InvalidIndex;
 	};
-	constexpr i32 i = sizeof( Node ) * 16;
+
+	template<typename _PRED>
+	inline index_t Interface::find_node( _PRED predicate ) const {
+		const size_t nodes_count = m_nodes.size();
+		for (index_t i = 0; i < nodes_count; i++)
+		{
+			if (predicate( m_nodes[ i ] ))
+				return i;
+		}
+		return InvalidIndex;
+	}
+
+	inline i32 Node::value() const noexcept {
+		return m_state & StateMask_Value;
+	}
+
+	inline float Node::value_fp() const noexcept {
+		const i32 val = value();
+		return *reinterpret_cast<const float *>(&val); // <- it works
+	}
 
 }
